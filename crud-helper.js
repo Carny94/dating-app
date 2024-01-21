e3f7a9383afa214caa04a11faf06b6e2516e0220
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2024-01-20 19:09:37 -0800

    starting dating app

---
+++ crud-helper.js
@@ -0,0 +1,13 @@
+// Connect to the database
+require('dotenv').config();
+require('./config/database');
+
+// Require the Mongoose models
+// const User = require('./models/user');
+// const Preference = require('./models/preference');
+// const Chat = require('./models/chat');
+
+
+// Local variables will come in handy for holding retrieved documents
+// let user, preference, chat;
+// let users, preferences, chats;
