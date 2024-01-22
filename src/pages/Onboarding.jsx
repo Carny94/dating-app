16e40b8a584a48bab39b18f7f6a65edda32de5a4
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2024-01-22 15:28:33 -0800

    halfway through onboarding form ui

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -3,6 +3,16 @@ import Nav from "../components/Nav"
 
 export default function Onboarding (){
 
+    const handleSubmit = () => {
+        console.log("submitted");
+
+    }
+
+    const handleChange = () => {
+        console.log("change");
+
+    }
+
     return (
         <>
             <Nav
@@ -56,38 +66,99 @@ export default function Onboarding (){
                             </div>
          
                             <label>Gender</label>
-                            <div className="multiple_input_container">
+                            <div className="multiple-input-container">
                             <input 
                                 type="radio" 
                                 id="man_gender_identity"
                                 name="gender_identity"
-                                value={"man"}
+                                value="man"
                                 onChange={handleChange}
                                 checked={false}
                             />
+                            <label htmlFor="man-gender-identity">Man</label>
                              <input 
                                 type="radio" 
                                 id="woman_gender_identity"
                                 name="gender_identity"
-                                value={"woman"}
+                                value="woman"
                                 onChange={handleChange}
                                 checked={false}
                             />
+                            <label htmlFor="woman-gender-identity">Woman</label>
                              <input 
                                 type="radio" 
                                 id="more_gender_identity"
                                 name="gender_identity"
-                                value={"more"}
+                                value="more"
                                 onChange={handleChange}
                                 checked={false}
                             />
+                            <label htmlFor="more-gender-identity">More</label>
                             </div>
+                            <label htmlFor="show-gender">Show gender on my profile</label>
+                            <input 
+                                type="radio" 
+                                id="show-gender"
+                                name="show_gender"
+                                onChange={handleChange}
+                                checked={false}
+                            />
+                            <label>Show Me</label>
+                            <div className="multiple-input-container">
+                            <input
+                                 type="radio" 
+                                 id="man-gender-interest"
+                                 name="gender_interest"
+                                 value="man"
+                                 onChange={handleChange}
+                                 checked={false}
+                             />
+                             <label htmlFor="man-gender-interest">Man</label>
+                              <input 
+                                 type="radio" 
+                                 name="gender_interest"
+                                 id="woman-gender-interest"                                 
+                                 value="woman"
+                                 onChange={handleChange}
+                                 checked={false}
+                             />
+                             <label htmlFor="woman-gender-interest">Woman</label>
+                              <input 
+                                 type="radio" 
+                                 name="gender_interest"
+                                 id="everyone-gender-interest"                                 
+                                 value="everyone"
+                                 onChange={handleChange}
+                                 checked={false}
+                             />
+                             <label htmlFor="everyone-gender-interest">Everyone</label>
+                         </div>
+                         <label htmlFor="about">About Me</label>
+                         <input
+                             type="text" 
+                             name="about"
+                             id="about"                                 
+                             value={""}
+                             required={true}
+                             placeholder="I like long walks along the beach..."
+                             onChange={handleChange}                        
+                             />
+                             <input type="submit" /> 
                     </section>
-
-
+                    <section>
+                    <label htmlFor="about">Profile Pic</label>
+                        <input
+                            type="url"
+                            name="url"
+                            id="url"
+                            onChange={handleChange}
+                            required={true}
+                            />
+                        <div className="photo-container">
+                    </div>
+                    </section>                  
                 </form>
-            </div>
-           
+            </div>          
         </>
     )
 }
